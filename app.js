eb25b4db2c93749df0f9d936851ee7fb4ece8d22
uabfelizardo <148287836+uabfelizardo@users.noreply.github.com>
2023-11-26 18:15:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
+require("dotenv").config();
+const { Sequelize } = require('sequelize');
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+
+// Replace these variables with your actual PostgreSQL database credentials
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  ssl: true, // Enable SSL/TLS
+  dialectOptions: {
+    ssl: {
+      require: true // Require SSL/TLS
+    }
+  }
+});
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.get("/", (req, res) => res.type('html').send(html));
 
