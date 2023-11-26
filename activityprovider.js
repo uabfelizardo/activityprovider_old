d789e4b6e3d0718eb0c6ef7787d043bb2047f652
uabfelizardo <148287836+uabfelizardo@users.noreply.github.com>
2023-11-27 00:39:35 +0200

    Add files via upload

---
+++ activityprovider.js
@@ -0,0 +1,10 @@
+[
+    {
+        "name": "Online Lab",
+        "config_url": "https://ap-webserver-online-lab.onrender.com/",
+        "json_params_url": "https://ap-webserver-online-lab.onrender.com/json_params_url.js",
+        "user_url": "https://ap-webserver-online-lab.onrender.com/deploy-atividade",
+        "analytics_url": "https://ap-webserver-online-lab.onrender.com/analytics-atividade",
+        "analytics_list_url": "https://ap-webserver-online-lab.onrender.com/analytics_list_url.js"
+    } 
+]
