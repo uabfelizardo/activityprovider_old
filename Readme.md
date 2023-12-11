b8a51e85476699ce9852dd3c8fed3479def46f7f
uabfelizardo <148287836+uabfelizardo@users.noreply.github.com>
2023-12-11 04:30:59 +0200

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Este projeto foi desenvolvido por Felizardo Chaguala no ambito do desenvolvimento de um Activity Provider na Unidade Curricular Arquitetura de Padroes de Software
+
+
+# Ver videos sobre sequelize
+https://www.youtube.com/watch?v=7udHlHLHUDw
+https://www.youtube.com/watch?v=TavTaLemiQo
+https://www.youtube.com/watch?v=LAK1XptdZX8&t=118s
+
+# Como criar uma API completa com NodeJS e Express | Backend - Projeto Full
+https://www.youtube.com/watch?v=Cdu0WJhI-d8&t=2134s
--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const { Sequelize,DataTypes } = require('sequelize');
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
-app.use(express.json());
+// const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser")
 
-// Substitua essas variáveis pelas suas 
-// credenciais reais do banco de dados PostgreSQL
+// app.engine('handlebars',handlebars({ defaultLayout: 'index'}))
+// app.set('view engine','handlebars');
+
+const port = process.env.PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(bodyParser.json())
+
+// credenciais do banco de dados PostgreSQL
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
   ssl: true, // Abilitar o  SSL/TLS
@@ -29,7 +37,7 @@ sequelize
   });
 
 // Modelo de dados
-const post = sequelize.define("obs",{
+const Obs = sequelize.define("obs",{
     idade:{
         type:DataTypes.INTEGER,
         allowNull:false
@@ -84,209 +92,104 @@ const post = sequelize.define("obs",{
     },
 });
 
+// Criar tabela usando sequelize
+// Obs.sync({force: true});
+
 // As observacoes sao as analiticas que o utente vai realizadas
 app.post("/create-obs", async(req,res) => {
-    const {idade,genero,tem_tosse_seca,tem_tosse_com_pus,
-           tem_tosse_com_sangue,tem_cansaco_excessivo,
-           tem_febre_baixa,tem_sudorese_noturna,tem_falta_de_apetite,
-           tem_palidez,tem_emagrecimento_acentuado,tem_rouquidao,
-           tem_fraqueza
-         }=req.body;
+    // Inserir analiticas na tabela
+      // Obs.create({
+      //     idade:req.body.idade,
+      //     genero:req.body.genero,
+      //     tem_tosse_seca:req.body.tem_tosse_seca,
+      //     tem_tosse_com_pus:req.body.tem_tosse_com_pus,
+      //     tem_tosse_com_sangue:req.body.tem_tosse_com_sangue,
+      //     tem_cansaco_excessivo:req.body.tem_cansaco_excessivo,
+      //     tem_febre_baixa:req.body.tem_febre_baixa,
+      //     tem_sudorese_noturna:req.body.tem_sudorese_noturna,
+      //     tem_falta_de_apetite:req.body.tem_falta_de_apetite,
+      //     tem_palidez:req.body.tem_palidez,
+      //     tem_emagrecimento_acentuado:req.body.tem_emagrecimento_acentuado,
+      //     tem_rouquidao:req.body.tem_rouquidao,
+      //     tem_fraqueza:req.body.tem_fraqueza
+      // }).then(function(){
+      //   res.send("Analitica registada com sucesso!")
+      // }).catch(function(err){
+      //   res.send("Erro: Ocorreu um erro no ato do registo de uma analitica." + err)
+      // });
 
-         try{
-            const newPost = await post.create({idade,genero,tem_tosse_seca,tem_tosse_com_pus,
-                tem_tosse_com_sangue,tem_cansaco_excessivo,
-                tem_febre_baixa,tem_sudorese_noturna,tem_falta_de_apetite,
-                tem_palidez,tem_emagrecimento_acentuado,tem_rouquidao,
-                tem_fraqueza})
-            
-                res.json(newPost);
-         
-            }catch(err){
-            console.log(err);
-         }
-});
-
-app.get("/get-obs-all", async(req,res) => {
-         try{
-
-            const allPosts = await post.findAll();
-            res.json(allPosts);
-         
+      try{
+          Obs.create({
+            idade:req.body.idade,
+            genero:req.body.genero,
+            tem_tosse_seca:"0",
+            tem_tosse_com_pus:"0",
+            tem_tosse_com_sangue:"0",
+            tem_cansaco_excessivo:"0",
+            tem_febre_baixa:"0",
+            tem_sudorese_noturna:"0",
+            tem_falta_de_apetite:"0",
+            tem_palidez:"0",
+            tem_emagrecimento_acentuado:"0",
+            tem_rouquidao:"0",
+            tem_fraqueza:"0"
+          }).then(function(){
+            // res.send("Analitica registada com sucesso!")
+            res.redirect("/get-analiticas")
+          }).catch(function(err){
+            res.send("Erro: Ocorreu um erro no ato do registo de uma analitica." + err)
+          });
         }catch(err){
-            console.log(err);
-         }
+          console.log(err);
+        }
+  // Testar se o codigo retorna o valor
+  // res.send("Idade : " + req.body.idade + "</br> Genero : " + req.body.genero + "</br> tem_tosse_seca: " + req.body.tem_tosse_seca)
 });
 
-app.get("/", (req, res) => res.type('html').send(html));
+// app.get("/get-analiticas", async(req,res) => {
+//         try{
+
+//           Obs.findAll().then(function(obs){
+//             // res.render('analiticas',{obs: obs})
+//             res.json(obs);
+//           });
+//             // const allPosts = await post.findAll();
+//             // res.json(allPosts);
+//         }catch(err){
+//             console.log(err);
+//         }
+// });
+
+app.get("/get-analiticas", async(req,res) => {
+  try{
+    //Retorna todas as analiticas feitas pelos utentes/doentes
+    Obs.findAll().then(function(obs){
+      res.json(obs);
+    });
+  }catch(err){
+      console.log(err);
+  }
+});
+
+// app.get("/", (req, res) => res.type('html').send(html));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + '/src/index.html');
+});
+
+app.get("/form-atividade", function(req, res){
+    res.sendFile(__dirname + '/src/form-atividade.html');
+});
+
+app.get("/deploy-atividade", function(req, res){
+    res.sendFile(__dirname + '/src/deploy-atividade.html');
+});
+
+app.get("/analiticas", function(req, res){
+    res.sendFile(__dirname + '/src/analiticas.html');
+});
 
 const server = app.listen(port, () => console.log(`Escutando apartir da port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html> 
-<html> 
-<head> 
-    <title> 
-        Online LAB 
-    </title> 
-    <style>
-    * {
-      box-sizing: border-box;
-    }
-
-    input[type=text], select, textarea {
-      width: 100%;
-      padding: 12px;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      resize: vertical;
-    }
-
-    label {
-      padding: 12px 12px 12px 0;
-      display: inline-block;
-    }
-
-    input[type=checkbox] {
-      background-color: #3CBC8D;
-      color: #3CBC8D;
-      padding: 12px 20px;
-      border: none;
-      border-radius: 4px;
-      cursor: pointer;
-      float: right;
-      text-align: left;
-    }
-
-    input[type=submit] {
-      background-color: #04AA6D;
-      color: white;
-      padding: 12px 20px;
-      border: none;
-      border-radius: 4px;
-      cursor: pointer;
-      float: right;
-    }
-
-    input[type=submit]:hover {
-      background-color: #45a049;
-    }
-
-    .container {
-      border-radius: 5px;
-      background-color: #f2f2f2;
-      padding: 20px;
-    }
-
-    .col-25 {
-      float: left;
-      width: 25%;
-      margin-top: 6px;
-    }
-
-    .col-75 {
-      float: left;
-      width: 75%;
-      margin-top: 6px;
-    }
-
-    /* Limpe os carros alegóricos após as colunas */
-    .row:after {
-      content: "";
-      display: table;
-      clear: both;
-    }
-
-    /* 
-      Layout responsivo - quando a tela tiver menos de 600 px 
-      de largura, empilhe as duas colunas uma sobre a outra, 
-      em vez de uma ao lado da outra 
-    */
-    @media screen and (max-width: 600px) {
-      .col-25, .col-75, input[type=submit] {
-        width: 100%;
-        margin-top: 0;
-      }
-    }
-    </style>
-</head> 
-
-<body style="text-align: left;"> 
-    <section>
-        Implementar um “Activity Provider” para auxiliar médico no rastreo e 
-        acompanhamento de utentes com o risco de contaminação com doenças respiratórias 
-        transmissíveis num laboratório remoto utilizando a Arquitetura Inven!RA.
-    </section>
-
-    <div class="p-2">
-      <form onsubmit="return handleData()" method="post" action="#">
-      
-      <label for="fname">Idade:</label>
-      <input type="text" id="idade" name="idade" value="38"><br>
-   
-      <label for="genero">Genero:</label>
-      <select id="genero" name="genero">
-        <option value="nulo"></option>
-        <option value="feminino">Feminino</option>
-        <option value="masculino">Masculino</option>
-      </select>
-
-    <h3>
-      Quais os sintomas tem?
-    </h3>
-
-      <div>
-        <input type="checkbox" name="utente" id="utente_tem_tosse_seca" value="tem_tosse_seca" checked> <label for="utente_tem_tosse_seca">Tosse seca</label>
-      </div>
-
-      <div>
-        <input type="checkbox" name="utente" id="utente_tem_tosse_com_pus" value="tem_tosse_com_pus"> <label for="utente_tem_tosse_com_pus">Tosse com pus</label>
-      </div>
-
-      <div>
-        <input type="checkbox" name="utente" id="utente_tem_tosse_com_sangue" value="tem_tosse_com_sangue"> <label for="utente_tem_tosse_com_sangue">Tosse com sangue</label>
-      </div>
-
-      <div>
-        <input type="checkbox" name="utente" id="utente_tem_cansaco_excessivo" value="tem_cansaco_excessivo"> <label for="utente_tem_cansaco_excessivo">Cansaco excessivo</label>
-      </div>
-
-      <div>
-        <input type="checkbox" name="utente" id="utente_tem_febre_baixa" value="tem_febre_baixa"> <label for="utente_tem_febre_baixa">Febre baixa</label>
-      </div>
-
-      <div>
-        <input type="checkbox" name="utente" id="utente_tem_sudorese_noturna" value="tem_sudorese_noturna"> <label for="utente_tem_sudorese_noturna">Sudores noturna</label>
-      </div>
-
-      <div>
-      <input type="checkbox" name="utente" id="utente_tem_falta_de_apetite" value="tem_falta_de_apetite" > <label for="utente_tem_falta_de_apetite">Falta de apetite</label>
-      </div>
-
-      <div>
-        <input type="checkbox" name="utente" id="utente_tem_palidez" value="tem_palidez"> <label for="utente_tem_palidez">Palidez</label>
-      </div>
-
-      <div>
-        <input type="checkbox" name="utente" id="utente_tem_emagrecimento_acentuado" value="tem_emagrecimento_acentuado"> <label for="utente_tem_emagrecimento_acentuado">Emagrecimento acentuado</label>
-      </div>
-
-      <div>
-        <input type="checkbox" name="utente" id="utente_tem_rouquidao" value="tem_rouquidao"> <label for="utente_tem_rouquidao">Rouquidao</label>
-      </div>
-
-      <div>
-        <input type="checkbox" name="utente" id="utente_tem_fraqueza" value="tem_fraqueza"> <label for="utente_tem_fraqueza">Fraqueza</label>
-      </div>
-
-      <div>
-        <input type="submit" name="submit" value="Submit"/>
-      </div>
-
-    <div>
-</body> 
-</html>
-`
